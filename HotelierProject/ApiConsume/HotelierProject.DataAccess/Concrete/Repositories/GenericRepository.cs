6cd6255e65d969c1554327c64d84bcd40d4da11e
Fırat Alçın <firatalcin04@gmail.com>
2023-05-07 14:03:16 +0300

    Generic Repository

---
+++ HotelierProject/ApiConsume/HotelierProject.DataAccess/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using HotelierProject.DataAccess.Abstract;
+using HotelierProject.DataAccess.Concrete.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelierProject.DataAccess.Concrete.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ProjectContext _context;
+
+        public GenericRepository(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+           _context.Add(entity);
+           _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+           _context.Remove(entity);
+           _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();  
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
