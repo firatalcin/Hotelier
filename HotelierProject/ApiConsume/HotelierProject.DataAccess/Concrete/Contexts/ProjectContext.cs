243c107b4d76790e2d04369e9ba55d4c754bc8ae
Fırat Alçın <firatalcin04@gmail.com>
2023-05-07 13:43:53 +0300

    Migration

---
+++ HotelierProject/ApiConsume/HotelierProject.DataAccess/Concrete/Contexts/ProjectContext.cs
@@ -0,0 +1,24 @@
+﻿using HotelierProject.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelierProject.DataAccess.Concrete.Contexts
+{
+    public class ProjectContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;initial catalog=HotelierDb;integrated security=true;");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
