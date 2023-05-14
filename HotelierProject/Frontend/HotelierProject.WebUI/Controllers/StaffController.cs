63f7627e8539ced63ee3e31a794618896a23d658
Fırat Alçın <firatalcin04@gmail.com>
2023-05-14 23:19:12 +0300

    Personellerin Güncellenmesi

--- HotelierProject/Frontend/HotelierProject.WebUI/Controllers/StaffController.cs
+++ HotelierProject/Frontend/HotelierProject.WebUI/Controllers/StaffController.cs
@@ -71,5 +71,19 @@ namespace HotelierProject.WebUI.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateStaff(UpdateStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5155/api/Staff", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
