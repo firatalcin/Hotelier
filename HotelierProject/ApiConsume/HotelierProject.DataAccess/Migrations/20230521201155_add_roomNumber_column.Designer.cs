620956df94e7ee112657666bf516ff91bdddbeb9
Fırat Alçın <firatalcin04@gmail.com>
2023-05-21 23:17:42 +0300

    Dto Sınıfları

--- HotelierProject/ApiConsume/HotelierProject.DataAccess/Migrations/20230521201155_add_roomNumber_column.Designer.cs
+++ HotelierProject/ApiConsume/HotelierProject.DataAccess/Migrations/20230521201155_add_roomNumber_column.Designer.cs
@@ -3,6 +3,7 @@ using HotelierProject.DataAccess.Concrete.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelierProject.DataAccess.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20230521201155_add_roomNumber_column")]
+    partial class add_roomNumber_column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HotelierProject/ApiConsume/HotelierProject.DataAccess/Migrations/20230521201155_add_roomNumber_column.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelierProject.DataAccess.Migrations
+{
+    public partial class add_roomNumber_column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "Rooms",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "Rooms");
+        }
+    }
+}
