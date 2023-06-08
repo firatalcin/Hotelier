509168718f35f2a135dd058c11e07569e7c8167e
Fırat Alçın <firatalcin04@gmail.com>
2023-06-08 23:54:54 +0300

    Identity Kütüphanesi

--- HotelierProject/ApiConsume/HotelierProject.DataAccess/Migrations/20230608205343_mig_add_identity.Designer.cs
+++ HotelierProject/ApiConsume/HotelierProject.DataAccess/Migrations/20230608205343_mig_add_identity.Designer.cs
@@ -4,6 +4,7 @@ using HotelierProject.DataAccess.Concrete.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelierProject.DataAccess.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20230608205343_mig_add_identity")]
+    partial class mig_add_identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
