ca4b422ff8dc0ad85cc0329303b50106beef6e41
Fırat Alçın <firatalcin04@gmail.com>
2023-06-20 23:32:30 +0300

    Register Dto

---
+++ HotelierProject/Frontend/HotelierProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using HotelierProject.Entities.Concrete;
+using HotelierProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelierProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateNewUserDto createNewUserDto)
+        {
+            return View();
+        }
+    }
+}
